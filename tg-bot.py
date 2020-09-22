8a91e38433ad94b22e29f47f631967054e010727
Sergey Kozyr <thelichking9@gmail.com>
2020-09-22 10:55:13 +0300

    Updated function and variable names

--- tg-bot.py
+++ tg-bot.py
@@ -44,7 +44,7 @@ def respond(bot, update):
   update.message.reply_text(response)
 
 
-def error(bot, update, error):
+def display_error(bot, update, error):
   logger.warning('Update "%s" caused error "%s"', update, error)
 
 
@@ -64,11 +64,11 @@ if __name__ == '__main__':
   logger.info('Бот tg-speech-recognition запущен')
 
   updater = Updater(tg_bot_token)
-  dp = updater.dispatcher
+  dispatcher = updater.dispatcher
 
-  dp.add_error_handler(error)
-  dp.add_handler(CommandHandler("start", start))
-  dp.add_handler(MessageHandler(Filters.text, respond))
+  dispatcher.add_error_handler(display_error)
+  dispatcher.add_handler(CommandHandler("start", start))
+  dispatcher.add_handler(MessageHandler(Filters.text, respond))
 
   while True:
 
